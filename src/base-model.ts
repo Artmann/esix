1c726993f7736cfc22da985be98f3208055ba984
Christoffer Artmann <Artgaard@gmail.com>
2020-09-10 16:19:49 +0200

    feat: Add API Documentation.

--- src/base-model.ts
+++ src/base-model.ts
@@ -9,10 +9,29 @@ export default class BaseModel {
   public id = '';
   public updatedAt: number | null = null;
 
+  /**
+   * Returns all models.
+   *
+   * Example
+   * ```
+   * const posts = await BlogPost.all();
+   * ```
+   */
   static async all<T extends BaseModel>(this: ObjectType<T>): Promise<T[]> {
     return new QueryBuilder(this).where({}).get();
   }
 
+  /**
+   * Creates a new model with the given attributes. The Id will be automatically generated
+   * if none is provided.
+   *
+   * Example
+   * ```
+   * const post = await BlogPost.create({ title: 'My First Blog Post!' });
+   * ```
+   *
+   * @param attributes
+   */
   static async create<T extends BaseModel>(this: ObjectType<T>, attributes: Dictionary): Promise<T> {
     const queryBuilder = new QueryBuilder(this);
 
@@ -28,36 +47,103 @@ export default class BaseModel {
     return model;
   }
 
+  /**
+   * Returns the model with the given id.
+   *
+   * Example
+   * ```
+   * const post = await BlogPost.find('5f5a41cc3eb990709eafda43');
+   * ```
+   *
+   * @param id
+   */
   static async find<T extends BaseModel>(this: ObjectType<T>, id?: string | number): Promise<T | null> {
     return new QueryBuilder(this).findOne({
       _id: id
     });
   }
 
+  /**
+   * Returns the first model matching where the `key` matches `value`.
+   *
+   * Example
+   * ```
+   * const user = await User.findBy('email', 'john.smith@company.com');
+   * ```
+   *
+   * @param key
+   * @param value
+   */
   static async findBy<T extends BaseModel>(this: ObjectType<T>, key: string, value: any): Promise<T | null> {
     return new QueryBuilder(this).findOne({
       [key]: value
     });
   }
 
+  /**
+   * Limits the number of models returned.
+   *
+   * @param length
+   */
   static limit<T extends BaseModel>(this: ObjectType<T>, length: number): QueryBuilder<T> {
     return new QueryBuilder<T>(this).limit(length);
   }
 
+  /**
+   * Specifies the order the models are returned.
+   *
+   * Example
+   * ```
+   * const posts = await BlogPost.orderBy('publishedAt', 'desc').get();
+   * ```
+   *
+   * @param key
+   * @param order
+   */
   static orderBy<T extends BaseModel>(this: ObjectType<T>,key: string, order: 'asc' | 'desc' = 'asc'): QueryBuilder<T> {
     return new QueryBuilder<T>(this).orderBy(key, order);
   }
 
+  /**
+   * Returns a QueryBuilder where `key` matches `value`.
+   *
+   * Example
+   * ```
+   * const posts = await BlogPost.where('status', 'published').get();
+   * ```
+   *
+   * @param key
+   * @param value
+   */
   static where<T extends BaseModel>(this: ObjectType<T>, key: string, value: any): QueryBuilder<T> {
     return new QueryBuilder(this).where(key, value);
   }
 
+  /**
+   * Returns models where `key` is in the array of `values`.
+   *
+   * Example
+   * ```
+   * const comments = await Comment.whereIn('postId', [1, 2, 3]).get();
+   * ```
+   *
+   * @param fieldName
+   * @param values
+   */
   static whereIn<T extends BaseModel>(this: ObjectType<T>, fieldName: string, values: any[]): QueryBuilder<T> {
     const queryBuilder = new QueryBuilder(this);
 
     return queryBuilder.whereIn(fieldName, values);
   }
 
+  /**
+   * Deletes the model from the database.
+   *
+   * Example
+   * ```
+   * await post.delete();
+   * ```
+   */
   async delete(): Promise<number> {
     const queryBuilder = new QueryBuilder(this.constructor as ObjectType<BaseModel>);
 
@@ -75,6 +161,18 @@ export default class BaseModel {
     return queryBuilder.where(foreignKey, (this as any)[localKey]);
   }
 
+  /**
+   * Persists the current changes to the database.
+   *
+   * Example
+   * ```
+   * const post = new Post();
+   *
+   * post.title = 'My Second Blog Post!';
+   *
+   * await post.save();
+   * ```
+   */
   async save(): Promise<void> {
     const queryBuilder = new QueryBuilder(this.constructor as ObjectType<BaseModel>);
 
