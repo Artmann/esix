7488b2671ee4d9bda59fbc2abc33b9e8b16a0c66
Christoffer Artmann <artgaard@gmail.com>
2026-05-08 12:09:13 +0200

    feat: export getCollectionName from public API
    Addresses PR review feedback: getCollectionName is now part of the
    public surface so external consumers don't need to reach into internal
    paths.

--- packages/esix/src/index.ts
+++ packages/esix/src/index.ts
@@ -1,7 +1,14 @@
 import BaseModel from './base-model'
 import { ConnectionHandler, connectionHandler } from './connection-handler'
+import { getCollectionName } from './naming'
 import QueryBuilder, { type Query } from './query-builder'
 import { type ObjectType, type Dictionary, type Document } from './types'
 
-export { BaseModel, QueryBuilder, ConnectionHandler, connectionHandler }
+export {
+  BaseModel,
+  ConnectionHandler,
+  QueryBuilder,
+  connectionHandler,
+  getCollectionName
+}
 export type { Query, ObjectType, Dictionary, Document }
